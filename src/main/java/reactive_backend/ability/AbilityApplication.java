2c9c451a2ddc1631bd1db2a6061bb4c099445bde
Jose Herrera <jose.herrera@pragma.com.co>
2025-04-04 11:32:21 -0500

    Initial commit

---
+++ src/main/java/reactive_backend/ability/AbilityApplication.java
@@ -0,0 +1,13 @@
+package reactive_backend.ability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbilityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbilityApplication.class, args);
+	}
+
+}
